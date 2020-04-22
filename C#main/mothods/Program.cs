b16f13246631402919f0a27d38b7d87b6a146db3
Matthew-swin <103219809@swin.edu.au>
2020-04-22 18:47:20 +1000

    cookie done did

---
+++ C#main/mothods/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace mothods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Welcome Friends!");
+            Console.WriteLine("Have a nice day!");
+            Console.ReadKey();
+        }
+    }
+}
